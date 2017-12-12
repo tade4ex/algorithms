0c46ad454442badb33a63fa064a8d49648f02066
root <root@debian.debian.local>
2017-12-13 00:02:47 +0100

    update lesson 5

--- lesson5/main.c
+++ lesson5/main.c
@@ -1,6 +1,7 @@
-#include "utilus.h"
+#include "utils.h"
 
 /* solution 1 */
+/* Реализовать перевод из 10 в 2 систему счисления с использованием стека. */
 typedef struct StackBin {
 	char text[100];
 	int n;
@@ -34,8 +35,51 @@ char * hexToBin (int hex) {
 }
 
 /* solution 2 */
+/*  Добавить в программу “реализация стека на основе односвязного списка”
+	проверку на выделение памяти. Если память не выделяется, то выводится
+	соответствующее сообщение. Постарайтесь создать ситуацию, когда память
+	не будет выделяться (добавлением большого количества данных).
+	*/
+typedef struct Node {
+	int val;
+	struct Node * next;
+} Node;
+
+typedef struct Stack2 {
+	Node *head;
+	int size;
+	int max_size;
+} Stack2;
+
+void push_stack2 (Stack2 * s, int val) {
+	if (s->size + 1 > s->max_size) {
+		printf("Error stack size");
+		return;
+	}
+	s->size++;
+	Node *new_node = (Node *) malloc(sizeof(Node));
+	new_node->val = val;
+	new_node->next = s->head;
+	s->head = new_node;
+}
+
+int pop_stack2 (Stack2 * s) {
+	s->size--;
+	if (s->size < -1) {
+		printf("Error stack size");
+		return -1;
+	}
+	int val = s->head->val;
+	s->head = s->head->next;
+	return val;
+}
 
 /* solution 3 */
+/*  Написать программу, которая определяет, является ли введенная скобочная
+	последовательность правильной. Примеры правильных скобочных выражений:
+	(), ([])(), {}(), ([{}]), неправильных — )(, ())({), (, ])}), ([(]) для скобок [,(,{.
+	Например: (2+(2*2)) или [2/{5*(4+7)}]
+	*/
 typedef struct StackEx {
 	char exs[100];
 	int n;
@@ -53,8 +97,6 @@ char t_pop_ex_stack (StackEx * s) {
 }
 
 bool solution3 (char * str) {
-	printf("%s\n", str);
-
 	StackEx s;
 	s.len = -1;
 	s.n = 0;
@@ -87,14 +129,171 @@ bool solution3 (char * str) {
 }
 
 /* solution 4 */
+/*  *Создать функцию, копирующую односвязный список
+	(то есть создает в памяти копию односвязного списка, без удаления первого списка).
+	*/
+char * copyChar (char * str) {
+	char * new_str = (char *) malloc(sizeof(char) * (strlen(str) + 1));
+	int i = 0;
+	while ((new_str[i] = str[i++]) != '\0');
+	return new_str;
+}
 
+/* solution 5 */
+/* **Реализовать алгоритм перевода из инфиксной записи арифметического выражения в постфиксную. */
+typedef struct NodeSolution5 {
+	char val;
+	struct NodeSolution5 * next;
+} NodeSolution5;
+
+typedef struct StackSolution5 {
+	NodeSolution5 * head;
+	int n;
+} StackSolution5;
+
+void push_stack_solution5 (StackSolution5 * s, char val) {
+	s->n++;
+	NodeSolution5 * new_node = (NodeSolution5 *) malloc(sizeof(NodeSolution5));
+	new_node->val = val;
+	new_node->next = s->head;
+	s->head = new_node;
+}
+
+char pop_stack_solution5 (StackSolution5 * s) {
+	s->n--;
+	if (s->n < -1) return '\0';
+	char val = s->head->val;
+	s->head = s->head->next;
+	return val;
+}
+
+char last_stack_solution5 (StackSolution5 * s) {
+	if (s->n <= -1) return '\0';
+	return s->head->val;
+}
+
+void check1_solution5_char (StackSolution5 * s, char * out, int * i) {
+	char c = last_stack_solution5(s);
+	if (c == '*' || c == '^') {
+		while (true) {
+			c = last_stack_solution5(s);
+			if (c == '-' || c == '+' || c == '(' || c == '\0') break;
+			out[*i++] = pop_stack_solution5(s);
+		}
+	}
+}
+
+char * solution5 (char * in) {
+	if (!solution3(in)) return "Error string incorrect";
+	char * out = (char *) malloc(sizeof(char) * (strlen(in) + 1));
+	StackSolution5 s;
+	s.n = -1;
+	char c;
+	int i = 0;
+	while ((c = *in++) != '\0') {
+		switch (c) {
+			case '(': push_stack_solution5(&s, c); break;
+			case ')':
+				while ((c = pop_stack_solution5(&s)) != '(')
+					out[i++] = c;
+				break;
+			case '+':
+				check1_solution5_char(&s, out, &i);
+				push_stack_solution5(&s, c);
+				break;
+			case '-':
+				check1_solution5_char(&s, out, &i);
+				push_stack_solution5(&s, c);
+				break;
+			case '*':
+				if (last_stack_solution5(&s) == '+') out[i++] = c;
+				else push_stack_solution5(&s, c);
+				break;
+			case '/':
+				check1_solution5_char(&s, out, &i);
+				push_stack_solution5(&s, c);
+				break;
+			case '^':
+				if (last_stack_solution5(&s) == '+') out[i++] = c;
+				else push_stack_solution5(&s, c);
+				break;
+			default: out[i++] = c;
+		}
+	}
+	while ((c = pop_stack_solution5(&s)) != '\0')
+		out[i++] = c;
+	out[i] = '\0';
+	return out;
+}
+
+/* solution 6 */
+/* *Реализовать очередь. */
+typedef struct QueueNode {
+	int id;
+	struct QueueNode * next;
+} QueueNode;
+
+typedef struct Queue {
+	QueueNode * head;
+	QueueNode * rear;
+	int len;
+} Queue;
+
+void push_to_queue (Queue * q, int id) {
+	QueueNode * new_node = (QueueNode *) malloc(sizeof(QueueNode));
+	new_node->id = id;
+	if (q->len == 0) q->head = new_node;
+	else if (q->len == 1) q->head->next = new_node;
+	else q->rear->next = new_node;
+	q->rear = new_node;
+	q->len++;
+}
+
+int pop_from_queue (Queue * q) {
+	if (q->len == 0) return 0;
+	q->len--;
+	int val = q->head->id;
+	q->head = q->head->next;
+	return val;
+}
+
+void solution6 () {
+	Queue q;
+	q.len = 0;
+
+	printf("\tIN:\n");
+	printf("1\n"); push_to_queue(&q, 1);
+	printf("2\n"); push_to_queue(&q, 2);
+	printf("3\n"); push_to_queue(&q, 3);
+	printf("4\n"); push_to_queue(&q, 4);
+
+	printf("\n\tOUT:\n");
+	printf("%d\n", pop_from_queue(&q));
+	printf("%d\n", pop_from_queue(&q));
+	printf("%d\n", pop_from_queue(&q));
+	printf("%d\n", pop_from_queue(&q));
+}
 
 int main () {
-
-	// printf("%d\n", solution3("(2+(2*2))"));
-	// printf("%d\n", solution3("(2+](2*2))"));
-
+	printf("\n\tSOLUTION 1:\n");
 	printf("%s\n", hexToBin(12));
 
-	
+	printf("\n\tSOLUTION 3:\n");
+	printf("%d\n", solution3("(2+(2*2))"));
+	printf("%d\n", solution3("(2+](2*2))"));
+
+	printf("\n\tSOLUTION 4:\n");
+	char * test = "a12b34";
+	char * new = copyChar(test);
+	printf("%s == %s\n", test, new);
+
+	printf("\n\tSOLUTION 5:\n");
+	printf("%s\n", solution5("(A+B)*C-(D-E)*(F+G)"));
+	printf("%s\n", solution5("3+4*2/(1-5)^2"));
+	printf("%s\n", solution5("3+4*2/((1-5)^2"));
+
+	printf("\n\tSOLUTION 6:\n");
+	solution6();
+
+	return 0;	
 }
--- lesson5/utils.h
+++ lesson5/utils.h
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <assert.h>
+#include <string.h>
 
 void t_swap(int *a, int *b) {
    *a = *a ^ *b;
